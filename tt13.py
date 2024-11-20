cd384be03682938a5c0233bbf90370b86c56949e
Akhil Rock Babu <146969874+akhilrockbabu@users.noreply.github.com>
2024-11-20 20:04:24 +0530

    Add files via upload

---
+++ tt13.py
@@ -0,0 +1,43 @@
+'''from collections import deque
+
+def bfs_shortest_path(graph,start,goal):
+    visited=set()
+    queue=deque([[start]])
+    print(deque)
+    while queue:
+        path=queue.popleft()
+        node=path[-1]
+
+        if node==goal:
+            return path
+        
+        elif node not in visited:
+            for neighbour in graph[node]:
+                new_path=list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+            visited.add(node)
+        print(queue)
+    return None
+
+graph={
+    '1':['2'],
+    '2':['3','5'],
+    '3':['4'],
+    '4':['5'],
+    '5':['6'],
+    '6':[]
+}
+
+shortest_path=bfs_shortest_path(graph,'1','6')
+print("shortest path : ",shortest_path)'''
+
+
+#github.com/zakbasil/Algorithms
+def cite(citations):
+    citations.sort()
+    for i in range(len(citations)):
+        if citations[i]==len(citations[i:]):
+            return citations[i]
+
+print(cite(list(map(int,input('enter the list').split()))))
